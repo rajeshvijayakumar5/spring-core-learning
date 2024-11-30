e25aa3ff2eba7797a81b872888723c479727e7b0
Rajesh <rajeshvijayakumar5@gmail.com>
2024-12-01 00:51:18 +0530

    Initial Commit

---
+++ spring-core-learning/src/main/java/com/rk/samples/spring/core/learning/SpringCoreLearning.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rk.samples.spring.core.learning;
+
+/**
+ *
+ * @author rajes
+ */
+public class SpringCoreLearning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
